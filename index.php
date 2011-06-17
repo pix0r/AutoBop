70443599194c9b055350d0079fd48c3a203cb3eb
Mike Matz <mike@pixor.net>
2011-06-17 11:37:52 -0700

    Added google analytics

--- index.php
+++ index.php
@@ -27,6 +27,17 @@ $js = file_get_contents($jsfile);
 	<br />
 	Code (<a href="<?php echo $jsfile ?>">raw file</a>):
 	<pre class="brush: js"><?php echo str_replace(array('<', '>'), array('&lt;', '&gt;'), $js); ?></pre>
+
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	try {
+		var pageTracker = _gat._getTracker("UA-432412-3");
+		pageTracker._trackPageview();
+	} catch(err) {}</script>
+
 </body>
 </html>
 
